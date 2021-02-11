f640975574f678435add34911fec596c8a1ac978
root <root@devmachine.us-central1-a.c.massive-carrier-303820.internal>
2021-02-11 23:11:58 +0000

    Ip Changes

--- src/test/java/servlet/createpage.java
+++ src/test/java/servlet/createpage.java
@@ -17,7 +17,7 @@ public class createpage extends HttpServlet {
 		try
 		{
 			Class.forName("org.postgresql.Driver");
-			String jdbcUrl="jdbc:postgresql://104.154.171.215:5432/postgres";
+			String jdbcUrl="jdbc:postgresql://34.122.18.103:5432/postgres";
 			String username="postgres";
 			String password="password";
 		
